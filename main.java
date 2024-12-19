eb9f46129fbf2e6eefd01e0d69ea04fae0e30ab6
PriyankaDev077 <ikori41@gmail.com>
2024-12-19 20:18:34 +0530

    p-hey hello

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args){
+System.out.println("1+1=2");
+System.out.println("4+6=10");
+System.out.println("16+6=18");
+System.out.println("77+1=78");
+}}
